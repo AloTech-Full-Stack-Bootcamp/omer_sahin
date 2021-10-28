5f97e6e14fa1d833118cad886dc39b9072faf598
Ömer Faruk ŞAHİN <79583574+omerfaruk-sahin@users.noreply.github.com>
2021-10-28 21:31:10 +0300

    Create Fs_modul.js

---
+++ 1_Hafta/Fs_modul.js
@@ -0,0 +1,22 @@
+const fs= require('fs');
+//mkdir
+fs.mkdir('ödev', ()=> {
+  console.log('Klasör başarıyla oluşturuldu.');
+});
+// write
+fs.writeFile('./ödev/employees.json', '{"name": "Employee 1 Name", "salary": 2000}', 'utf8', (err)=> {
+    if (err) console.log(err);
+    console.log("Dosya oluşturuldu.")
+})
+// Read
+fs.readFile('employees.json',(err,data)=>{
+    if (err) console.log(err);
+    console.log(JSON.parse(data))
+    console.log("Dosya okundu")
+})
+// update
+fs.truncate('employees.json', 0,()=>{});
+fs.appendFile('employees.json','{"name": "Employee 2 Name", "salary": 5000}' ,'utf8',(err)=>{
+    if (err) console.log(err);
+    console.log("Dosya güncellendi")
+})
